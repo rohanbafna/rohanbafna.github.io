37cf6db21c5205b7c8fff8564d175692973efd62
Rohan Bafna <130247393+rohanbafna@users.noreply.github.com>
2026-05-13 18:01:58 -0400

    Add serve command to flake

--- flake.nix
+++ flake.nix
@@ -7,14 +7,21 @@
 
   outputs =
     { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      gems = pkgs.bundlerEnv {
+        name = "website-gems";
+        gemdir = ./.;
+      };
+    in
     {
+      apps.x86_64-linux.default = {
+        type = "app";
+        program = "${pkgs.writeShellScript "website-serve" "${gems}/bin/jekyll serve"}";
+        meta.description = "Serve the website on localhost";
+      };
       devShells.x86_64-linux.default =
         let
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          gems = pkgs.bundlerEnv {
-            name = "website-gems";
-            gemdir = ./.;
-          };
           # a text file containing the paths to the flake inputs in order to stop
           # them from being garbage collected
           # source: https://gist.github.com/tpwrules/34db43e0e2e9d0b72d30534ad2cda66d
