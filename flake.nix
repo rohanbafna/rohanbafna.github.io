ecb5405b0c3cf393baea2a87dab5369a3a5411b0
Rohan Bafna <130247393+rohanbafna@users.noreply.github.com>
2026-05-13 17:51:56 -0400

    Cache the flake input (nixpkgs) too

--- flake.nix
+++ flake.nix
@@ -15,9 +15,18 @@
             name = "website-gems";
             gemdir = ./.;
           };
+          # a text file containing the paths to the flake inputs in order to stop
+          # them from being garbage collected
+          # source: https://gist.github.com/tpwrules/34db43e0e2e9d0b72d30534ad2cda66d
+          pleaseKeepMyInputs = pkgs.writeTextDir "bin/.please-keep-my-inputs" (
+            builtins.concatStringsSep " " [ nixpkgs ]
+          );
         in
         pkgs.mkShell {
-          buildInputs = [ gems ];
+          buildInputs = [
+            gems
+            pleaseKeepMyInputs
+          ];
         };
     };
 }
